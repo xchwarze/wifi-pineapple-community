6fab18170f7c8bf880a5bbaad5c91e57937e205e
DSR! <xchwarze@gmail.com>
2023-10-27 02:43:50 -0300

    [MODULES][DWall] Release version 1.5

--- modules/src/DWall/api/module.php
+++ modules/src/DWall/api/module.php
@@ -21,12 +21,8 @@ class DWall extends Module
     {
         $this->disable();
 
-        if (!file_exists("/usr/lib/libpcap.so.1.3") && file_exists("/usr/lib/libpcap.so")) {
-            symlink("/usr/lib/libpcap.so", "/usr/lib/libpcap.so.1.3");
-        }
-        
-        $this->execBackground("/usr/bin/python /pineapple/modules/DWall/assets/DWall.py");
-        $this->execBackground("/pineapple/modules/DWall/assets/http_sniffer br-lan");
+        $this->execBackground("python /pineapple/modules/DWall/assets/DWall.py");
+        $this->execBackground("/usr/sbin/http_sniffer br-lan");
         $this->response = array("success" => true);
     }
 
@@ -39,7 +35,7 @@ class DWall extends Module
 
     private function getStatus()
     {
-        if (trim(exec("ps -aux | grep [D]Wall.py")) != "" && trim(exec("ps -aux| grep [h]ttp_sniffer")) != "") {
+        if (trim(exec("ps -aux | grep [D]Wall.py")) != "" && trim(exec("ps -aux | grep [h]ttp_sniffer")) != "") {
             $this->response = array("running" => true);
         } else {
             $this->response = array("running" => false);
